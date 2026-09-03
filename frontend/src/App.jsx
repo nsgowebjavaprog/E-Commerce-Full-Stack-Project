4b6be35ca4f9d94864ee07e7c79f91bf4c1e4d0b
ns loni <nsloni@gmail.com>
2026-09-03 18:33:53 +0530

    Full Fronend setup and code will done..!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,29 @@
-import ProductList from "../pages/ProductList";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import ProductList from "./pages/ProductList";
+import ProductDetails from "./pages/ProductDetails";
+import Nabvar from './components/Navbar';
+import CartPage from './pages/CartPage';
+import CheckoutPage from './pages/CheckoutPage';
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import PrivateRouter from './components/PrivateRouter';
 
-function App(){
-  return (
-    <div>
-        <ProductList/>
-    </div>
-  );
+function App() {
+    return (
+        <Router>
+            <Nabvar/>
+            <Routes>
+                <Route path="/" element={<ProductList/>}/>
+                <Route path="/product/:id" element={<ProductDetails/>}/>
+                <Route path="/cart" element={<CartPage/>}/>
+                <Route element={<PrivateRouter/>}>
+                    <Route path="/checkout" element={<CheckoutPage/>}/>
+                </Route>
+                <Route path="/login" element={<Login/>} />
+                <Route path="/signup" element={<Signup/>} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
